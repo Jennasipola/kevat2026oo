5d8bdc1a9c2710b1bcbace34310ec51a973905c4
Jennasipola <sipolajenna@gmail.com>
2026-02-02 00:29:30 +0200

    Update main.cpp

--- viikko1/viikkotehtava1/main.cpp
+++ viikko1/viikkotehtava1/main.cpp
@@ -32,3 +32,90 @@ int main(){
 
     return 0;
 }
+
+//osa2
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int game() {
+    srand(time(0));
+    int salainenLuku = rand() % 20 + 1;
+    int arvattuLuku;
+    int arvausKerrat = 0;
+
+    do {
+        cout << "Arvaa luku 1-20 valilta: ";
+        cin >> arvattuLuku;
+
+        arvausKerrat++;
+
+        if (arvattuLuku > salainenLuku) {
+            cout << "Luku on liian suuri, kokeile uudestaan!" << endl;
+        } else if (arvattuLuku < salainenLuku) {
+            cout << "Luku on liian pieni, kokeile uudestaan!" << endl;
+        } else {
+            cout << "Oikein arvattu!" << endl;
+        }
+
+    } while (arvattuLuku != salainenLuku);
+
+    return arvausKerrat;
+}
+
+int main() {
+    int tulos = game();
+    cout << "Arvasit luvun oikein " << tulos << " yrityksellä!" << endl;
+
+    cout << "Paina Enter sulkeaksesi ohjelman...";
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
+
+//osa3
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int game(int maxnum) {
+    int salainenLuku = rand() % maxnum + 1;
+    int arvattuLuku;
+    int arvausKerrat = 0;
+
+    do {
+        cout << "Arvaa luku 1-" << maxnum << " valilta: ";
+        cin >> arvattuLuku;
+
+        arvausKerrat++;
+
+        if (arvattuLuku > salainenLuku) {
+            cout << "Luku on liian suuri, kokeile uudestaan!" << endl;
+        } else if (arvattuLuku < salainenLuku) {
+            cout << "Luku on liian pieni, kokeile uudestaan!" << endl;
+        } else {
+            cout << "Oikein arvattu!" << endl;
+        }
+
+    } while (arvattuLuku != salainenLuku);
+
+    return arvausKerrat;
+}
+int main() {
+    srand(time(0));
+
+    int tulos = game(40);
+
+    cout << "Arvasit luvun oikein " << tulos << " yrityksella" << endl;
+
+    cout << "Paina Enter sulkeaksesi ohjelman...";
+    cin.ignore();
+    cin.get();
+
+    return 0;
